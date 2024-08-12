ad46ddf7a52338d7b328fbc46f938077262de1aa
ZaRaaaZa <ivanuk_iana@mail.ru>
2024-08-12 17:15:30 +0300

    Add files via upload

---
+++ ZaRaaaZa1dz.py
@@ -0,0 +1,24 @@
+print("Hello world")
+
+"1st program"
+
+print(9 ** 0.5 * 5)
+
+"2nd program"
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+"3rd program"
+
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+"4th program"
+
+print('123.456')
+print(type('123.456'))
+print(str('123.456'))
+print(type(123.456))
+y = float(123.456)
+print(int((y * 10) % 10))
